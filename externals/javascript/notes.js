d70390d844d43f9f85e07d28c4bebcb1d96bc7ac
dkbozkurt <dkaanbozkurt@gmail.com>
2023-08-06 17:51:39 +0200

    course base push

---
+++ externals/javascript/notes.js
@@ -0,0 +1,12 @@
+// #region Functions
+
+// Number Obj
+// .toFixed(n), this converts the Number to a String with n digits following the decimal point.
+
+function milesToKilometers(dist)
+{
+    return (dist*1.609).toFixed(1);
+}
+
+milesToKilometers(5); // 8.0
+// #endregion
---
+++ externals/javascript/utilities.js
@@ -0,0 +1,11 @@
+const PI = 3.1416;
+
+function degToRads(angle) // 360 = 2PI
+{
+    return angle / 180 * PI;
+};
+
+function milesToKilometers(miles)
+{
+    return miles * 1.609;
+};
