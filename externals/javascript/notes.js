a8ee4660a56413e0a97849779a63663d69b62a15
dkbozkurt <dkaanbozkurt@gmail.com>
2023-08-06 18:09:01 +0200

    notes update

--- externals/javascript/notes.js
+++ externals/javascript/notes.js
@@ -1,12 +1,14 @@
-// #region Functions
-
-// Number Obj
 // .toFixed(n), this converts the Number to a String with n digits following the decimal point.
-
 function milesToKilometers(dist)
 {
     return (dist*1.609).toFixed(1);
 }
-
 milesToKilometers(5); // 8.0
-// #endregion
+
+
+// isNaN(a) is a built-in js function to test if a is not a number.
+isNaN(9) // true;
+
+
+// Math.random() return a pseudo random number between 0 and 1
+Math.random();
